6a682c1331d3592f97344e81481b2a3e07b8770f
no1uknow <nobe1976@users.noreply.github.com>
2015-02-12 15:36:19 -0500

    Update and rename cm.mk to aosp_jfltevzw.mk
    We dont build CM...

--- aosp_jfltevzw.mk
+++ aosp_jfltevzw.mk
@@ -1,10 +1,5 @@
 $(call inherit-product, device/samsung/jfltevzw/full_jfltevzw.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltevzw \
@@ -14,6 +9,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
-PRODUCT_NAME := cm_jfltevzw
+PRODUCT_NAME := aosp_jfltevzw
 PRODUCT_DEVICE := jfltevzw
 
