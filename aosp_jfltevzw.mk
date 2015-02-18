6774ed7d8383d70e358e156bfb26126d1f42ed6c
no1uknow <nobe1976@users.noreply.github.com>
2015-02-18 15:49:14 -0500

    Removed some duplicate stuff

--- aosp_jfltevzw.mk
+++ aosp_jfltevzw.mk
@@ -1,12 +1,9 @@
 $(call inherit-product, device/samsung/jfltevzw/full_jfltevzw.mk)
 
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-  PRODUCT_NAME=jfltevzw \
-  TARGET_DEVICE=jfltevzw \
   BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.4.2/KOT49H/I545VRUFNK1:user/release-keys" \
   PRIVATE_BUILD_DESC="jfltevzw-user 4.4.2 KOT49H I545VRUFNK1 release-keys"
-
+  
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
 PRODUCT_NAME := aosp_jfltevzw
